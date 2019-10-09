1cef7032522dd6f20e6f7780882977bf5482b818
Michael Bryan <michaelfbryan@gmail.com>
2019-10-09 22:22:53 +0800

    Added more tests

--- src/Decoder.test.ts
+++ src/Decoder.test.ts
@@ -1,4 +1,5 @@
 import { Decoder, Packet } from ".";
+import { ChecksumFailed } from "./Decoder";
 
 expect.extend({
     toEqualPacket: function (received, other) {
@@ -26,5 +27,31 @@ describe("Decoder", function () {
         const got = decoder.decode();
 
         expect(got).toEqualPacket(packet);
+    })
+
+    it("Can detect a single-character message", function () {
+        const decoder = new Decoder();
+        const packet = new Packet(1, "!");
+
+        decoder.pushPacket(packet);
+        const got = decoder.decode();
+
+        expect(got).toEqualPacket(packet);
+    })
+
+    it("Will remove the packet header when there's a checksum failure", function () {
+        const decoder = new Decoder();
+        const packet = new Packet(1, "!");
+        const buffer = packet.encoded();
+        buffer[buffer.length - 1] ^= 0x0f;
+        decoder.push(buffer);
+        const errors: ChecksumFailed[] = [];
+        decoder.on("crc-error", e => errors.push(e));
+
+        const got = decoder.decode();
+
+        expect(got).toEqual(null);
+        expect(errors).toHaveLength(1);
+        expect(decoder.bufferLength).toEqual(1);
     })
 })
